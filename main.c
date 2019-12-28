7d873b013daaa9e9c41c56c43512d64a07e5fc61
leandroljpa97 <leandro.pereira@tecnico.ulisboa.pt>
2019-12-28 16:19:13 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,32 @@
+/**********************************************************************
+Data Mining Project
+Authors:  Leandro Almeida, K-5600
+          Arnim Kumar
+
+
+File name: main.c
+COMMENTS
+**********************************************************************/
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[]) {
+
+printf("ola\n");
+
+    
+
+	return 0;
+}
